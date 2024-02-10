10d1bee4148d89bbf286e3570439eae0fb389979
Luiz <l.guilherme128@gmail.com>
2024-02-10 11:22:50 -0300

    Using bits for firnd duplicates and anagrams

--- Program.cs
+++ Program.cs
@@ -6,13 +6,28 @@ class Program
     {
         //c++ you use \o for break reference in char arrays
         Strings strings = new() { str = ['l', 'U', 'i', 'Z', ' ', '$', 'h', 'O', 'w', ' ', ' ', ' ', 'a', '\t'] };
+        Strings str = new() { str = ['l', 'U', 'i', 'z', 'z', '$', '\t'] };
+        Strings str2 = new() { str = ['m', 'e', 'd', 'i', 'c', 'a', 'l', '\t'] };
+        Strings str3 = new() { str = ['d', 'e', 'c', 'i', 'm', 'a', 'l', '\t'] };
         strings.StrLength();
         // strings.ToUpper();
-        strings.Count();
-        strings.CountWhiteSpace();
-        strings.HasSpecialCharacter(ref strings);
+        // str.ToUpper();
+        // strings.Count();
+        // strings.CountWhiteSpace();
+        // strings.HasSpecialCharacter(ref strings);
         // strings.ReverseString(ref strings);
-        strings.ReverseString2(ref strings);
+        // strings.ReverseString2(ref strings);
+
+        // strings.StartsWithOptimized(str.str);
+
+        // strings.CheckPalindrome();
+
+        // strings.FindDuplicate(str);
+        // str.FindDuplicateHash(str);
+        // str.FindDuplicateInBits(str);
+        // str.CheckForAnagramHash(str2, str3);
+        str.CheckForAnagramAsBits(str2, str3);
+
 
 
 
--- Struct/Strings.cs
+++ Struct/Strings.cs
@@ -17,8 +17,13 @@ struct Strings
         int i;
         for (i = 0; str[i] != '\t'; i++)
         {
-            str[i] = (char)((int)str[i] - 32);
+            CheckCase(ref str[i]);
         }
+    }
+    static void CheckCase(ref char str)
+    {
+        if (str > 'Z' && str <= 'z')
+            str = (char)((int)str - 32);
     }
 
     public void ToggleCases()
@@ -101,6 +106,7 @@ struct Strings
 
     }
 
+
     public void ReverseString(ref Strings strings)
     {
         int i;
@@ -143,6 +149,204 @@ struct Strings
             System.Console.WriteLine(strings.str[i]);
         }
 
+    }
+
+    //i think is a good idea always use this with tuUpper or lower
+    public void StartsWith(char[] chars)
+    {
+        int i, j;
+
+        for (i = 0, j = 0; str[i] != '\t' && chars[j] != '\t'; i++, j++)
+        {
+            System.Console.WriteLine(str[i]);
+            // if (str[i] != chars[j]) break;
+            //using ASCII values instead directly char
+            if (str[i] == chars[j]) System.Console.WriteLine("Do something is equal here");
+            else if (str[i] >= chars[j]) System.Console.WriteLine("Income string is smaller than the stored one");
+            else System.Console.WriteLine("Incoming string is bigger");
+        }
+    }
+
+    // i will make a more optimal version where i don't use ToUpper on startsWith
+    public void StartsWithOptimized(char[] chars)
+    {
+        int i, j;
+        for (i = 0, j = 0; str[i] != '\t' && chars[j] != '\t'; i++, j++)
+        {
+            CheckCase(ref str[i]);
+            CheckCase(ref chars[j]);
+            if (str[i] == chars[j]) System.Console.WriteLine("Do something is equal here");
+            else if (str[i] >= chars[j]) System.Console.WriteLine("Income string is smaller than the stored one");
+            else System.Console.WriteLine("Incoming string is bigger");
+        }
+    }
+
+    public void CheckPalindrome()
+    {
+        char[] chars = ['m', 'a', 'd', 'a', 'm'];
+        for (int i = 0, j = chars.Length - 1; i <= j; i++, j--)
+        {
+            System.Console.WriteLine(chars[i] == chars[j]);
+        }
+    }
+
+    public int Max(char[] st)
+    {
+        int max = st[0];
+        int i;
+        for (i = 0; st[i] != '\t'; i++)
+        {
+            if (max < st[i]) max = st[i];
+        }
+        return max;
+    }
+
+    //@@@@@@@@@@@@@@@@ challenges
+    public void FindDuplicate(Strings strings)
+    {
+        for (int i = 0; strings.str[i] != '\t'; i++)
+        {
+            for (int j = i + 1; strings.str[j] != '\t'; j++)
+            {
+                if (strings.str[i] == strings.str[j])
+                    strings.str[j] = '0';
+            }
+            System.Console.WriteLine(strings.str[i]);
+        }
+
+    }
+
+    //i definitely should use another approach
+    // public void FindDuplicateHash(Strings strings)
+    // {
+    //     int[] chars = new int['A' + 'z'];
+    //     int i;
+    //     for (i = 0; strings.str[i] != '\t'; i++)
+    //     {
+    //         if (strings.str[i] >= 'A' && strings.str[i] <= 'Z')
+    //             chars[strings.str[i] - 'A']++;
+    //         else if (strings.str[i] >= 'a' && strings.str[i] <= 'z')
+    //             chars[strings.str[i] - 'a']++;
+    //     }
+    //     chars[i] = '\t';
+    //     for (i = 0; i < chars.Length; i++)
+    //     {
+    //         System.Console.WriteLine(i + );
+    //         System.Console.WriteLine(chars[i]);
+    //     }
+    // }
+    //i will use only  upper
+    public void FindDuplicateHash(Strings strings)
+    {
+        int[] chars = new int['Z' - 'A' + 1];
+        int i;
+        for (i = 0; strings.str[i] != '\t'; i++)
+        {
+            if (!Alphabet(strings.str[i]) && !NumASCII(strings.str[i]) && strings.str[i] != ' ') break;
+            CheckCase(ref strings.str[i]);
+            chars[strings.str[i] - 'A']++;
+        }
+        string output = "";
+        for (i = 0; i < chars.Length; i++)
+        {
+            // System.Console.WriteLine(i + 'A');
+            // System.Console.WriteLine(chars[i]);
+            if (chars[i] > 1)
+            {
+                output += (char)(i + 'A');
+            }
+        }
+        System.Console.WriteLine(output);
+    }
+
+    //Now we gonna start use bits
+
+    public void FindDuplicateInBits(Strings strings)
+    {
+        //c# already declare as int 32 bit here, so the size in bit will be enough for
+        //store values between 65 to 97 as ASCII 
+        int H = 0, x;
+        int i;
+        for (i = 0; strings.str[i] != '\t'; i++)
+        {
+            CheckCase(ref strings.str[i]);
+            x = 1;
+            // x = x << (strings.str[i] - 97);
+            //left shifting this (strings.str[i] - 97) amount of time 
+            x <<= strings.str[i] - 97;
+            if ((x & H) > 0)
+            {
+                System.Console.WriteLine(strings.str[i]);
+            }
+            else H = x | H;
+        }
+    }
+
+    public void CheckForAnagramHash(Strings strings, Strings strings1)
+    {
+        //check if the words has exactly the same amount of index and same letters as well
+        //does not matter if the words is different. Like decimal and medical 
+
+        // Remember about lower case here i'm to lazy right now to operate
+        if (strings.str.Length != strings1.str.Length) return;
+        int i;
+        int[] chars = new int['z' - 'a'];
+        for (i = 0; strings.str[i] != '\t' && strings1.str[i] != '\t'; i++)
+        {
+            chars[strings.str[i] - 'a']++;
+            chars[strings1.str[i] - 'a']--;
+        }
+        string output = "";
+        for (i = 0; i < chars.Length; i++)
+        {
+            // System.Console.WriteLine(i + 'A');
+            // System.Console.WriteLine(chars[i]);
+            if (chars[i] < 0)
+            {
+                output += (char)(i + 'a');
+                break;
+            }
+        }
+        //the result for anagram should be "" otherwise will print the different letters
+        System.Console.WriteLine(output);
+    }
+
+    //this one just works if there is no DUPLICATED letters in the same string
+    public void CheckForAnagramAsBits(Strings strings, Strings strings1)
+    {
+        //check if the words has exactly the same amount of index and same letters as well
+        //does not matter if the words is different. Like decimal and medical 
+        int H = 0, J = 0, x;
+        int i;
+        for (i = 0; strings.str[i] != '\t' && strings1.str[i] != '\t'; i++)
+        {
+            CheckCase(ref strings.str[i]);
+            x = 1;
+            // x = x << (strings.str[i] - 97);
+            //left shifting this (strings.str[i] - 97) amount of time 
+            x <<= strings.str[i] - 97;
+            if ((x & H) > 0)
+            {
+                System.Console.WriteLine(strings.str[i]);
+            }
+            else H = x | H;
+
+            x = 1;
+            x <<= strings1.str[i] - 97;
+
+            if ((x & J) > 0)
+            {
+                System.Console.WriteLine(strings1.str[i]);
+            }
+            else J = x | J;
+        }
+        System.Console.WriteLine(H);
+        System.Console.WriteLine(J);
+        //if is equal means the bits are allocated in same places so anagram condition check
+        if (H == J) { H = 0; J = 0; }
+        System.Console.WriteLine(H);
+        System.Console.WriteLine(J);
+        // System.Console.WriteLine(H);
     }
 
 }
